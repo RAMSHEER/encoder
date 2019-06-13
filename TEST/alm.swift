41bb13a0d1bf1a10d8b7547d9eba215656bf4b76
Ramsheer T <stratagile@stratagiles-mbp>
2019-06-14 00:07:45 +0530

    nsobject class

---
+++ TEST/alm.swift
@@ -0,0 +1,33 @@
+////
+////  alm.swift
+////  TEST
+////
+////  Created by Stratagile on 14/06/19.
+////  Copyright © 2019 ramsheer. All rights reserved.
+////
+//
+//import Foundation
+//import SwiftyJSON
+//
+//class doaResponseClass {
+//    
+//    var DoaId :Int32
+//    var Title : String?
+//    var ArabicLanguage : String?
+//    var EnglishLanguage : String?
+//    var MalayLanguage : String?
+//    var DoaImagePath : String?
+//    
+//    init(doaResponseDetails: JSON) {
+//        
+//        self.DoaId = doaResponseDetails["DoaId"].int32!
+//        self.Title = doaResponseDetails["Title"].stringValue
+//        self.ArabicLanguage = doaResponseDetails["ArabicLanguage"].stringValue
+//        self.EnglishLanguage = doaResponseDetails["EnglishLanguage"].stringValue
+//        self.MalayLanguage = doaResponseDetails["MalayLanguage"].stringValue
+//        self.DoaImagePath = doaResponseDetails["DoaImagePath"].stringValue
+//        
+//    }
+//    
+//}
+//
